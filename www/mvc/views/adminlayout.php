d7a6da0e2edcfb8a564d24be79eebbdf9c5ae281
Tan Thi <tanthi.dev@gmail.com>
2022-11-02 19:56:33 +0700

    add famework tailwind

--- www/mvc/views/adminlayout.php
+++ www/mvc/views/adminlayout.php
@@ -39,7 +39,8 @@
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-		<link rel="stylesheet" href="/style.css">
+		<link rel="stylesheet" href="/css/style.css">
+        <link rel="stylesheet" href="/css/tailwind.css">
 		<link rel="stylesheet" href="/mvc/data/font/fontawesome-free-6.2.0-web/fontawesome-free-6.2.0-web/css/all.min.css">
 		<link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;300;400;500;700&family=Qwitcher+Grypen:wght@700&display=swap" rel="stylesheet">
 		<title>Carlton Hotel</title>
